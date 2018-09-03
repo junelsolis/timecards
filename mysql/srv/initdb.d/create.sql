678c44dd84066b1f631061e0061bd7966f7310fc
Junel Solis <junel.solis@gmail.com>
2018-09-03 07:37:15 +0300

    commit

---
+++ mysql/srv/initdb.d/create.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 172.100.0.101
+-- Generation Time: May 22, 2018 at 12:23 PM
+-- Server version: 5.7.22
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `radius`
+--
+CREATE DATABASE IF NOT EXISTS `timecards` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `timecards`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
